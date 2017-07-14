e5a80fcf22536378fbc86e4eeaec4e5e3b17da92
Andrew Stanton <up683693@myport.ac.uk>
2017-07-14 19:10:44 +0100

    Added webpack to prepare client code
    Built code will not be added to the repo

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/browserClient/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, './public/js'),
+	},
+	watch: true,
+};
