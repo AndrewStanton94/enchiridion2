5fcd9aa479ac5f72adc808de724173cf6467c55b
Andrew Stanton <up683693@myport.ac.uk>
2017-07-28 22:48:06 +0100

    Tidied code
    Removed unneeded test code and placeholder content

--- src/browserClient/index.js
+++ src/browserClient/index.js
@@ -1,17 +1,3 @@
-import creator from './creator';
-import fragment from './fragment';
-import testData from './testData';
-
-// creator.get(testData.creator._id);
-// creator.make(testData.creator.newUserData);
-creator.search(testData.creator.searchTerm);
-// creator.update(testData.creator._id, testData.creator.userUpdate);
-
-// fragment.get(testData.fragment._id);
-// fragment.make(testData.fragment.newFragmentData);
-fragment.search(testData.fragment.searchTerm);
-// fragment.update(testData.fragment._id, testData.fragment.fragmentUpdate);
-
 import setUp from './setUp';
 
 window.addEventListener('load', () => {
